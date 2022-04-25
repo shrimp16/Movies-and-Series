25c49657b1babcb6ae66b6c77647ed227863e6ce
Shrimp <lcamara0916@gmail.com>
2022-04-25 14:47:47 +0000

    Started making a little strucutre for the auth part of the server

---
+++ SERVER/Users/auth.js
@@ -0,0 +1,14 @@
+const fileManager = require('./fileManager');
+
+module.exports = {
+    register: () => {
+        console.log("wip");
+    },
+    login: () => {
+        console.log("wip");
+    }
+}
+
+function usernameExists(){
+    console.log("wip")
+}
