064b9336fe0a70beef2a9a335e6799ad29a53d2a
Shrimp <lcamara0916@gmail.com>
2022-04-28 11:43:08 +0000

    Implemented route to create more content and added uploadRouter to the main.js

--- SERVER/Routers/uploadRouter.js
+++ SERVER/Routers/uploadRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../Storage/storageManager');
+const jsonParser = require('body-parser').json();
 
 const usersManager = require('../Users/users');
 const fileManager = require('../Users/fileManager');
@@ -11,5 +12,17 @@ router.post('/file/', upload.single('image'), (req, res) => {
 
 })
 
+router.post('/add-content/:id', jsonParser, (req, res) => {
+
+    let users = fileManager.getUsers();
+    let content = req.body.content;
+
+    users[req.params.id].content.push(content);
+
+    fileManager.updateFile(users);
+
+    res.send("Added content with success!");
+
+})
 
 module.exports = router;
--- SERVER/main.js
+++ SERVER/main.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const upload = require('./Storage/storageManager');
 const app = express();
+
 const usersRouter = require('./Routers/usersRouter');
+const uploadRouter = require('./Routers/uploadRouter');
 
 app.listen(12345, () => {
     console.log("server running");
 })
 
 app.use(usersRouter);
+app.use(uploadRouter);
