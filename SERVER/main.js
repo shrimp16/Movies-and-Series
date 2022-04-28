7c8a0a91692e3de5c892914b6f8ff86131ba2877
Shrimp <lcamara0916@gmail.com>
2022-04-28 11:45:07 +0000

    Added the page router to main.js

--- SERVER/main.js
+++ SERVER/main.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+const PORT = 50000;
+
+const pageRouter = require('./Routers/pageRouter');
 const usersRouter = require('./Routers/usersRouter');
 const uploadRouter = require('./Routers/uploadRouter');
 
-app.listen(12345, () => {
-    console.log("server running");
+app.listen(PORT, () => {
+    console.log(`Server running on Port: ${PORT}`);
 })
 
+app.use(pageRouter);
 app.use(usersRouter);
 app.use(uploadRouter);
