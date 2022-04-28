8c79f9bc6e4acb4e39001bd6477bd3832e20fff9
Shrimp <lcamara0916@gmail.com>
2022-04-28 11:55:00 +0000

    Implemented the page router and changed the web-page directory in order to make it more accessible from the router

--- SERVER/Routers/pageRouter.js
+++ SERVER/Routers/pageRouter.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.sendFile(__dirname + '/WEB-PAGE/index.html');
+})
+
 module.exports = router;
--- SERVER/main.js
+++ SERVER/main.js
@@ -11,6 +11,8 @@ app.listen(PORT, () => {
     console.log(`Server running on Port: ${PORT}`);
 })
 
+app.use(express.static(__dirname + '/Routers/WEB-PAGE'));
+
 app.use(pageRouter);
 app.use(usersRouter);
 app.use(uploadRouter);
