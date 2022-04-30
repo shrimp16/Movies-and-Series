bf8da16366231165c2a707a44190636ed7be1290
Shrimp <lcamara0916@gmail.com>
2022-04-30 09:56:53 +0000

    Added some fake data to the database to make some tests

--- SERVER/db.sql
+++ SERVER/db.sql
@@ -1,2 +1,5 @@
 CREATE TABLE users(userID int AUTO_INCREMENT, username VARCHAR(255) UNIQUE, password VARCHAR(255), PRIMARY KEY(userID))
 CREATE TABLE content(contentID int AUTO_INCREMENT, title VARCHAR(255), rate int, image VARCHAR(255), ownerID int, PRIMARY KEY(contentID), FOREIGN KEY(ownerID) REFERENCES users(userID))
+
+INSERT INTO users SET username = 'user', password = 'pass'
+INSERT INTO content SET title = 'Title', rate = 10, image = 'Name of the image', ownerID = 1
