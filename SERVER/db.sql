11a4f40da76ab8f0bf8c47d3cd0a2be25a623bef
Shrimp <lcamara0916@gmail.com>
2022-04-30 09:51:57 +0000

    Wrote down the sql to create the tables for the database

---
+++ SERVER/db.sql
@@ -0,0 +1,2 @@
+CREATE TABLE users(userID int AUTO_INCREMENT, username VARCHAR(255) UNIQUE, password VARCHAR(255), PRIMARY KEY(userID))
+CREATE TABLE content(contentID int AUTO_INCREMENT, title VARCHAR(255), rate int, image VARCHAR(255), ownerID int, PRIMARY KEY(contentID), FOREIGN KEY(ownerID) REFERENCES users(userID))
