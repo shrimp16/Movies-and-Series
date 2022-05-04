b36c4481518953fd2e7a4d4e3467b2a4d766d461
Shrimp <lcamara0916@gmail.com>
2022-05-04 11:19:24 +0000

    Minor changed to the database

--- SERVER/Persistance/db.sql
+++ SERVER/Persistance/db.sql
@@ -1,5 +1,16 @@
-CREATE TABLE users(userID int AUTO_INCREMENT, username VARCHAR(255) UNIQUE, password VARCHAR(255), userImage VARCHAR(255), PRIMARY KEY(userID))
-CREATE TABLE content(contentID int AUTO_INCREMENT, title VARCHAR(255), rate int, image VARCHAR(255), ownerID int, PRIMARY KEY(contentID), FOREIGN KEY(ownerID) REFERENCES users(userID))
+CREATE TABLE users(userID int AUTO_INCREMENT,
+username VARCHAR(255) UNIQUE, 
+password VARCHAR(255),
+userImage VARCHAR(255),
+PRIMARY KEY(userID))
 
-INSERT INTO users SET username = 'user', password = 'pass'
+CREATE TABLE content(contentID int AUTO_INCREMENT,
+title VARCHAR(255),
+rate int,
+image VARCHAR(255),
+ownerID int,
+PRIMARY KEY(contentID),
+FOREIGN KEY(ownerID) REFERENCES users(userID))
+
+INSERT INTO users SET username = 'user', password = 'pass', picture = "default"
 INSERT INTO content SET title = 'Title', rate = 10, image = 'Name of the image', ownerID = 1
