5ca38fe8619cce1dd4df76b971bb87a1a0c29aa0
Shrimp <lcamara0916@gmail.com>
2022-05-05 12:46:06 +0000

    Deleted pointless route

--- SERVER/Routers/uploadRouter.js
+++ SERVER/Routers/uploadRouter.js
@@ -5,19 +5,11 @@ const jsonParser = require('body-parser').json();
 
 const dbManager = require('../Users/Persistance/dbManager');
 
-router.post('/file', upload.single('image'), (req, res) => {
+router.post('/add-content', upload.single('image'), (req, res) => {
 
     console.log(req.file.buffer.toString('base64'));
     console.log(req.body.thing);
     res.send("xd");
-
-})
-
-router.post('/add-content', jsonParser, (req, res) => {
-
-    dbManager.addContent(req.body.content);
-
-    res.send("Added content with success!");
 
 })
 
