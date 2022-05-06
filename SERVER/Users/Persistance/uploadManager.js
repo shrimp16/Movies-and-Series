4310c211c75bb79d7e21d61d950f420589e65895
Shrimp <lcamara0916@gmail.com>
2022-05-06 11:49:47 +0000

    Deleted old multer config

--- SERVER/Users/Persistance/uploadManager.js
+++ SERVER/Users/Persistance/uploadManager.js
@@ -1,16 +1,5 @@
 const multer = require('multer');
 
-/*const fileStorageEngine = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, '../SERVER/Storage/IMAGES');
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    }
-})
-
-const upload = multer({storage: fileStorageEngine});*/
-
 const upload = multer({storage:multer.memoryStorage()});
 
 module.exports = upload;
