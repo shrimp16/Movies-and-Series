89c3783a6beb8c5473beb980c720113b4be0f070
Shrimp <lcamara0916@gmail.com>
2022-05-16 17:28:50 +0000

    Fixed error if user didn't input username or password

--- SERVER/Routers/WEB-PAGE/scripts/pages/controllers/login.js
+++ SERVER/Routers/WEB-PAGE/scripts/pages/controllers/login.js
@@ -16,11 +16,25 @@ export default class Login {
 
     preparePage() {
         document.getElementById('submit-login').addEventListener('click', () => {
+
+            let usernameInput = document.querySelector('#login-un');
+            let passwordInput = document.querySelector('#login-pw');
+
+            if(!usernameInput.value){
+                alert("Please insert username!");
+                return;
+            }
+
+            if(!passwordInput.value){
+                alert("Please insert password!");
+                return;
+            }
+
             fetch('http://192.168.1.103:50000/login', {
                 method: 'POST',
                 body: JSON.stringify({
-                    'username': document.querySelector('#login-un').value,
-                    'password': document.querySelector('#login-pw').value
+                    'username': usernameInput.value,
+                    'password': passwordInput.value
                 }),
                 headers: {
                     'Content-type': 'application/json; charset=UTF-8'
