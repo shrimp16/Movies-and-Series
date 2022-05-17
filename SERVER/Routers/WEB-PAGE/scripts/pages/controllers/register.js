b7640ea0c4c3d032ab03a06ae922037ad539c10b
Shrimp <lcamara0916@gmail.com>
2022-05-17 10:54:33 +0000

    Added some keyboard navigation to the register page

--- SERVER/Routers/WEB-PAGE/scripts/pages/controllers/register.js
+++ SERVER/Routers/WEB-PAGE/scripts/pages/controllers/register.js
@@ -1,4 +1,31 @@
 export default class Register{
     constructor(){
+        this.setupElements();
+    }
+
+    setupElements() {
+        let usernameInput = document.querySelector('#register-un');
+        let emailInput = document.querySelector('#register-email');
+        let passwordInput = document.querySelector('#register-pw');
+
+        usernameInput.addEventListener('keypress', (e) => {
+            if(e.key === "Enter") {
+                passwordInput.select();
+            }
+        })
+
+        passwordInput.addEventListener('keypress', (e) => {
+            if(e.key === "Enter") {
+                emailInput.select();
+            }
+        })
+
+        emailInput.addEventListener('keypress', (e) => {
+            if(e.key === "Enter"){
+                document.getElementById('submit-register').click();
+            }
+        })
+
+        
     }
 }
