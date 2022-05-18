c136bf1a389796a4ae1a306d2480eae722a2e076
Shrimp <lcamara0916@gmail.com>
2022-05-18 15:15:01 +0000

    Now when a user enters his own profile it creates 3 buttons

--- SERVER/Routers/WEB-PAGE/scripts/pages/controllers/profiles.js
+++ SERVER/Routers/WEB-PAGE/scripts/pages/controllers/profiles.js
@@ -1,22 +1,28 @@
 export default class Profile {
     constructor(id){
         console.log(id);
-        this.loadProfile(id);
+        this.verifyProfile(id);
     }
 
-    loadProfile(id) {
+    verifyProfile(id) {
         if(id === localStorage.getItem('userID') || id === sessionStorage.getItem('userID')){
             alert('Loading user profile');
+            this.loadOwnProfile();
         }else {
             alert('Loading someone else profile');
         }
     }
 
-    loadOwnProfile() {
+    loadProfile() {
 
     }
 
-    loadAnyProfile() {
-        
+    loadOwnProfile() {
+        const HTML = `<div class="user-profile-panel">
+        <button class="blue-purple-btn" id="new-show">New Show</button>
+        <button class="blue-purple-btn" id="edit-profile">Edit Profile</button>
+        <button class="blue-purple-btn" id="log-out">Log out</button>
+        </div>`;
+        document.getElementById('profile-header').innerHTML = document.getElementById('profile-header').innerHTML + HTML;
     }
 }
