49002581bb876bd7a13f0f43eb662926aff27723
Shrimp <lcamara0916@gmail.com>
2022-05-19 11:20:36 +0000

    Started implementing the script for the profile body

--- SERVER/Routers/WEB-PAGE/scripts/pages/controllers/profiles.js
+++ SERVER/Routers/WEB-PAGE/scripts/pages/controllers/profiles.js
@@ -1,6 +1,6 @@
 export default class Profile {
     constructor(id){
-        console.log(id);
+        this.body = document.querySelector('#profile-body')
         this.verifyProfile(id);
     }
 
@@ -11,11 +11,35 @@ export default class Profile {
         }else {
             alert('Loading someone else profile');
         }
+        this.loadProfile();
     }
 
     loadProfile() {
+        let newCard = document.createElement('div');
+        newCard.classList.add('card');
+
+        let img = new Image();
+        img.src = 'https://picsum.photos/500?random=1';
+
+        let cardText = document.createElement('div');
+        cardText.classList.add('card-title');
+        cardText.innerText = 'Lorem ipsum dolot sit.'
+
+        newCard.appendChild(img);
+        newCard.appendChild(cardText);
+
+        this.body.appendChild(newCard);
 
     }
+
+
+    /*<div class="card">
+        <img src="https://picsum.photos/500?random=1" alt="">
+        <div class="card-title">
+            Lorem ipsum dolor sit.
+        </div>
+    </div>
+    */
 
     loadOwnProfile() {
         const HTML = `<div class="user-profile-panel">
