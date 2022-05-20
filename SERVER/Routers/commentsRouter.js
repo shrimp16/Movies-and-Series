2f2f31eb992663304e1847840fb342cc0f8b1743
Shrimp <lcamara0916@gmail.com>
2022-05-20 16:24:18 +0000

    Created a file for the routes related to the comments

---
+++ SERVER/Routers/commentsRouter.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const jsonParser = require('body-parser').json();
+const dbManager = require('../Users/Persistance/dbManager');
+
+router.post('/add-comment', jsonParser, (req, res) => {
+
+    const comment = {
+        ownerID: req.body.ownerID,
+        contentID: req.body.contentID,
+        text: req.body.text,
+        liked: 0
+    }
+
+    dbManager.addComment(comment);
+    res.send("Sucess");
+
+})
+
+router.delete('/remove-comment/:id', (req, res) => {
+    dbManager.removeComment(req.params.id);
+    res.send("Sucess");
+})
