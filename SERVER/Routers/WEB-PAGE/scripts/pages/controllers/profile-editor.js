68f602ce3190916afb09e4c34b3747e1819ca741
Shrimp <lcamara0916@gmail.com>
2022-05-22 10:59:04 +0000

    Micro refactor

--- SERVER/Routers/WEB-PAGE/scripts/pages/controllers/profile-editor.js
+++ SERVER/Routers/WEB-PAGE/scripts/pages/controllers/profile-editor.js
@@ -1,26 +1,26 @@
 export default class ProfileEditor {
     constructor() {
+        this.bannerInput = document.getElementById('banner-input');
+        this.profilePicInput = document.getElementById('profile-pic-input');
         this.setupElements();
     }
 
     setupElements() {
 
         document.getElementById('profile-pic-btn').addEventListener('click', () => {
-            document.getElementById('profile-pic-input').click();
+            this.profilePicInput.click();
         })
 
         document.getElementById('banner-btn').addEventListener('click', () => {
-            document.getElementById('banner-input').click();
+            this.bannerInput.click();
         })
         
-        document.getElementById('profile-pic-input').addEventListener('change', (e) => {
-            console.log(document.getElementById('profile-pic-input').files);
-            document.getElementById('profile-picture-edit').src = URL.createObjectURL(document.getElementById('profile-pic-input').files[0])
+        document.getElementById('profile-pic-input').addEventListener('change', () => {
+            document.getElementById('profile-picture-edit').src = URL.createObjectURL(this.profilePicInput.files[0])
         })
 
-        document.getElementById('banner-input').addEventListener('change', (e) => {
-            console.log(document.getElementById('banner-input').files);
-            document.getElementById('banner-edit').src = URL.createObjectURL(document.getElementById('banner-input').files[0])
+        document.getElementById('banner-input').addEventListener('change', () => {
+            document.getElementById('banner-edit').src = URL.createObjectURL(this.bannerInput.files[0])
         })
     }
 }
