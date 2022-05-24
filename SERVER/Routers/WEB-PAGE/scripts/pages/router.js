600da519ba2e61fc9b0fb7c6aa0eb7352fac0418
Shrimp <lcamara0916@gmail.com>
2022-05-24 12:13:14 +0000

    Delete old switch

--- SERVER/Routers/WEB-PAGE/scripts/pages/router.js
+++ SERVER/Routers/WEB-PAGE/scripts/pages/router.js
@@ -25,42 +25,6 @@ export default class Router {
 
         const loader = await import(`./controllers/${hash}.js`);
         new loader.default();
-
-        /*switch (hashArr[0]) {
-            case '':
-                //home page
-                break;
-            case 'login':
-                await this.loadPage('login');
-                const loader = await import('./controllers/login.js');
-                new loader.default();
-                /*import('./controllers/login.js')
-                .then((loader) => {
-                    new loader.default();
-                })
-                break;
-            case 'register':
-                await this.loadPage('register');
-                new Register();
-                break;
-            case 'recovery':
-                await this.loadPage('recovery');
-                break;
-            case 'profile':
-                await this.loadPage('profile');
-                new Profile(hashArr[1]);
-                break;
-            case 'show-creator':
-                await this.loadPage('show-creator');
-                break;
-            case 'profile-editor':
-                await this.loadPage('profile-editor');
-                new ProfileEditor();
-                break;
-            default:
-                await this.loadPage('404');
-                new NotFound();
-        }*/
     }
 
     async loadPage(page) {
