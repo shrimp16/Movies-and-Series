d4a9bbd4da74a8354e707373268dd2dfffe5d494
Shrimp <lcamara0916@gmail.com>
2022-05-25 14:11:03 +0000

    The in real time update of the preview is currently working but only on the title

--- SERVER/Routers/WEB-PAGE/scripts/pages/controllers/show-creator.js
+++ SERVER/Routers/WEB-PAGE/scripts/pages/controllers/show-creator.js
@@ -1,14 +1,20 @@
 export default class ShowCreator{
 
     constructor(){
+
         this.titleInput = document.getElementById('show-creator-title-input');
         this.descriptionInput = document.getElementById('show-creator-description-input');
         this.rateInput = document.getElementById('show-creator-rate-input');
         this.uploadInputBtn = document.getElementById('show-upload-btn');
         this.upload = document.getElementById('show-creator-image-input');
+
+        this.titlePreview = document.getElementById('show-creator-title');
+        this.descriptionPreview = document.getElementById('show-creator-description');
+        this.ratePreview = document.getElementById('show-creator-rate');
+        this.imagePreview = document.getElementById('show-creator-image');
+
         this.setupElements();
 
-        this.show = {};
         document.title = 'My Shows List | Show Creator';
     }
 
@@ -22,10 +28,11 @@ export default class ShowCreator{
             if(e.key === 'Enter'){
                 console.log("xdxd");
             }
+
             setTimeout(() => {
-                this.show.title = this.titleInput.value;
-                console.log(this.show);
+                this.titlePreview.innerText = this.titleInput.value;
             }, 1)
+
         })
     }
 }
