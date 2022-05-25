ae1b7c22f64e88137ca4812d27be5dba901e217b
Shrimp <lcamara0916@gmail.com>
2022-05-25 14:07:32 +0000

    Made some tryouts for in time update of the preview

--- SERVER/Routers/WEB-PAGE/scripts/pages/controllers/show-creator.js
+++ SERVER/Routers/WEB-PAGE/scripts/pages/controllers/show-creator.js
@@ -1,11 +1,31 @@
 export default class ShowCreator{
 
     constructor(){
+        this.titleInput = document.getElementById('show-creator-title-input');
+        this.descriptionInput = document.getElementById('show-creator-description-input');
+        this.rateInput = document.getElementById('show-creator-rate-input');
+        this.uploadInputBtn = document.getElementById('show-upload-btn');
+        this.upload = document.getElementById('show-creator-image-input');
         this.setupElements();
+
+        this.show = {};
         document.title = 'My Shows List | Show Creator';
     }
 
     setupElements(){
+        this.uploadInputBtn.addEventListener('click', () => {
+            this.upload.click();
+        })
 
+        this.titleInput.addEventListener('keydown', (e) => {
+
+            if(e.key === 'Enter'){
+                console.log("xdxd");
+            }
+            setTimeout(() => {
+                this.show.title = this.titleInput.value;
+                console.log(this.show);
+            }, 1)
+        })
     }
 }
