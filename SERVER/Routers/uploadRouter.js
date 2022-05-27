f0043b24c41053d34cbde1cd7a2a4fc31e3d7744
Shrimp <lcamara0916@gmail.com>
2022-05-27 12:53:03 +0000

    The server now stores the content corretly on the database

--- SERVER/Routers/uploadRouter.js
+++ SERVER/Routers/uploadRouter.js
@@ -5,28 +5,24 @@ const path = require('path');
 
 const dbManager = require('../Persistance/dbManager');
 
-let file;
-
 router.post('/add-content', upload.single('image'), (req, res) => {
 
     let newContent = {
         ownerID: req.body.ownerID,
         title: req.body.title,
         text: req.body.text,
-        rate: req.body.rate
-        //image: req.file.buffer.toString('base64')
+        rate: req.body.rate,
+        image: req.file.filename
     }
 
-    console.log(req.file);
-    file = req.file.filename;
-    //dbManager.addContent(newContent);
+    dbManager.addToDatabase('content', newContent);
 
     res.send("Added content!");
 
 })
 
-router.get('/test-file', (req, res) => {
+/*router.get('/test-file', (req, res) => {
     res.sendFile(path.join(__dirname, `../Persistance/Images/${file}`));
-})
+})*/
 
 module.exports = router;
