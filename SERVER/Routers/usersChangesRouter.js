e6d33b799698755758a10895fbd0098614980336
Shrimp <lcamara0916@gmail.com>
2022-06-01 13:04:49 +0000

    Added the routes to the correct file

--- SERVER/Routers/usersChangesRouter.js
+++ SERVER/Routers/usersChangesRouter.js
@@ -2,3 +2,20 @@ const express = require('express');
 const router = express.Router();
 const jsonParser = require('body-parser').json();
 
+const dbManager = require('../Persistance/dbManager');
+const upload = require('../Persistance/uploadManager');
+
+router.post('/update-profile-picture/:id', upload.single('image'), (req, res) => {
+    dbManager.editDataFromDatabase('profiles', `picture="${req.file.filename}"`, `userID="${req.params.id}"`);
+    res.send('Changed profile picture');
+})
+
+router.post('/update-profile-banner/:id', upload.single('image'), (req, res) => {
+    dbManager.editDataFromDatabase('profiles', `banner="${req.file.filename}"`, `userID="${req.params.id}"`);
+    res.send('Changed profile banner');
+})
+
+router.post('/update-description/:id', jsonParser, (req, res) => {
+    dbManager.editDataFromDatabase('profiles', `description="${req.body.description}"`, `userID="${req.params.id}"`);
+    res.send('Changed description');
+})
