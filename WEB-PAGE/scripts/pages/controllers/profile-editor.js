6ca8564afbc08625b9f01d64bd48a644057a6d38
Shrimp <lcamara0916@gmail.com>
2022-06-02 14:00:53 +0000

    Implemented a way to update the profile on the front end

--- WEB-PAGE/scripts/pages/controllers/profile-editor.js
+++ WEB-PAGE/scripts/pages/controllers/profile-editor.js
@@ -2,6 +2,7 @@ export default class ProfileEditor {
     constructor() {
         this.bannerInput = document.getElementById('banner-input');
         this.profilePicInput = document.getElementById('profile-pic-input');
+        this.descriptionInput = document.getElementById('description-input');
         this.setupElements();
         document.title = 'My Shows List | Profile Editor';
     }
@@ -15,13 +16,51 @@ export default class ProfileEditor {
         document.getElementById('banner-btn').addEventListener('click', () => {
             this.bannerInput.click();
         })
-        
+
         document.getElementById('profile-pic-input').addEventListener('change', () => {
             document.getElementById('profile-picture-edit').src = URL.createObjectURL(this.profilePicInput.files[0])
         })
 
         document.getElementById('banner-input').addEventListener('change', () => {
             document.getElementById('banner-edit').src = URL.createObjectURL(this.bannerInput.files[0])
+        })
+
+        document.getElementById('submit-profile-edit').addEventListener('click', () => {
+
+            let formData = new FormData();
+
+            console.log(this.descriptionInput.value);
+
+            if (this.profilePicInput.files[0] && this.bannerInput.files[0]) {
+                formData.append('picture', true);
+                formData.append('banner', true);
+                formData.append('images', this.profilePicInput.files[0]);
+                formData.append('images', this.bannerInput.files[0]);
+            }
+
+            if (!this.profilePicInput.files[0] && this.bannerInput.files[0]) {
+                formData.append('banner', true);
+                formData.append('images', upload2.files[0]);
+            }
+
+            if (this.profilePicInput.files[0] && !this.bannerInput.files[0]) {
+                formData.append('picture', true);
+                formData.append('images', this.profilePicInput.files[0]);
+            }
+
+            if (this.descriptionInput.value) {
+                formData.append('description', this.descriptionInput.value);
+            }
+
+            fetch(`http://192.168.1.103:50000/update-profile/${sessionStorage.userID || localStorage.userID}`, {
+                method: 'POST',
+                body: formData
+            })
+                .then(response => response.text()
+                    .then((response) => {
+                        console.log(response);
+                    }))
+
         })
     }
 }
