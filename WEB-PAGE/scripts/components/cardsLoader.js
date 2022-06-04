1890ea4ed71db458e5d70e04c844af9389ffa440
Shrimp <lcamara0916@gmail.com>
2022-06-04 11:36:22 +0000

    Started working on a class that will contain all the code related to loading the cards

---
+++ WEB-PAGE/scripts/components/cardsLoader.js
@@ -0,0 +1,54 @@
+let cards = [];
+
+export default class CardsLoader{
+
+    constructor(){
+        this.body = document.getElementById('profile-body');
+    }
+
+    async getCards(id){
+        let cont = 0;
+        await fetch(`http://192.168.1.103:50000/user-content/${id}`)
+            .then(response => response.json()
+                .then(async (shows) => {
+                    cards = shows;
+                    for await (const show of shows) {
+                        fetch(`http://192.168.1.103:50000/image/${show.image}`)
+                            .then(image => image.blob())
+                            .then((image) => {
+                                cards[cont].blob = image;
+                                this.createCard(cards[cont].blob, show.title);
+                                cont++;
+                            })
+                    }
+                }))
+    }
+
+    loadCards(cards) {
+        this.body.innerHTML = '';
+        for (let i = 0; i < cards.length; i++){
+            this.createCard(cards[i].blob, cards[i].title);
+        }
+    }
+
+    createCard(image, title){
+        let newCard = document.createElement('div');
+        newCard.classList.add('card');
+
+        let img = new Image();
+        img.src = URL.createObjectURL(image);
+
+        let cardText = document.createElement('div');
+        cardText.classList.add('card-title');
+        cardText.innerText = title;
+
+        newCard.appendChild(img);
+        newCard.appendChild(cardText);
+
+        this.appendCard(newCard);
+    }
+
+    appendCard(card) {
+        this.body.appendChild(card);
+    }
+}
