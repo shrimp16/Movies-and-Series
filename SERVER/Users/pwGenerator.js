e1270414c15897bad88168c2b2f65b5eaecccb67
Shrimp <lcamara0916@gmail.com>
2022-06-09 13:53:19 +0000

    Finished the implementation for the password generator

--- SERVER/Users/pwGenerator.js
+++ SERVER/Users/pwGenerator.js
@@ -1,9 +1,16 @@
 const CHAR = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*()'
 
-let string;
+module.exports = {
+    generate: () => {
+        let characters = CHAR;
+        let password;
 
-for(let i = 0; i < 14; i++){
-    string += CHAR.charAt(Math.floor(Math.random() * CHAR.length));
+        for(let i = 0; i < 14; i++){
+            let char = characters.charAt(Math.floor(Math.random() * characters.length))
+            password += char;
+            characters.replace(char, '');
+        }
+
+        return password
+    }
 }
-
-console.log(string);
