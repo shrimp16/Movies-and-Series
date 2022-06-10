2f94d2f40a70ab2c3889e2cb8a25901428022bc2
Shrimp <lcamara0916@gmail.com>
2022-06-10 12:09:47 +0000

    Started implementing the script to change password on the front end

--- WEB-PAGE/scripts/pages/controllers/password-changer.js
+++ WEB-PAGE/scripts/pages/controllers/password-changer.js
@@ -1,5 +1,23 @@
 export default class PasswordChanger {
     constructor(){
-        
+        this.passwordInput = document.getElementById('new-password-input');
+        this.id = sessionStorage.getItem('userID') || localStorage.getItem('userID');
+        this.setupElements();
+    }
+
+    setupElements(){
+        console.log(this.id);
+        document.getElementById('submit-new-password').addEventListener('click', () => {
+            fetch(`http://192.168.1.103:50000/change-password/${this.id}`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    password: this.passwordInput.value
+                })
+            })
+            .then(response => response.text()
+            .then((response) => {
+                console.log(response);
+            }))
+        })
     }
 }
