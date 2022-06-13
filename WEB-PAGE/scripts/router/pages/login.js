2a376fd978325693f2af2936282510345765c631
Shrimp <lcamara0916@gmail.com>
2022-06-13 13:44:46 +0000

    Login page fully working

--- WEB-PAGE/scripts/router/pages/login.js
+++ WEB-PAGE/scripts/router/pages/login.js
@@ -28,5 +28,80 @@ export default class Login {
         `
 
         this.body.innerHTML = HTML;
+        this.setupElements();
     }
+
+    setupElements() {
+        console.log("xd");
+        let usernameInput = document.getElementById('login-un');
+        let passwordInput = document.getElementById('login-pw');
+
+        usernameInput.addEventListener('keypress', (e) => {
+
+            if (e.key === 'Enter') {
+                passwordInput.select();
+            }
+
+        })
+
+        passwordInput.addEventListener('keypress', (e) => {
+
+            if (e.key === 'Enter') {
+                document.getElementById('submit-login').click()
+            }
+
+        })
+
+        document.getElementById('submit-login').addEventListener('click', () => {
+
+            if (!usernameInput.value) {
+                alert('Please insert username!')
+                return;
+            }
+
+            if (!passwordInput.value) {
+                alert('Please insert password!');
+                return;
+            }
+
+            fetch('http://192.168.1.103:50000/login', {
+                method: 'POST',
+                body: JSON.stringify({
+                    'username': usernameInput.value,
+                    'password': passwordInput.value
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8'
+                }
+            }).then(response => response.text()
+                .then((response) => {
+
+                    let id = parseInt(response);
+
+                    if (!isNaN(id)) {
+                        this.success(id, usernameInput.value);
+                    } else {
+                        alert(response);
+                    }
+
+                }))
+        })
+    }
+
+    success(id, username) {
+
+        document.getElementById('auth-btn').innerHTML = username;
+
+        if (document.querySelector('#save-user').checked) {
+            localStorage.setItem('username', username);
+            localStorage.setItem('userID', id);
+        } else {
+            sessionStorage.setItem('username', username);
+            sessionStorage.setItem('userID', id);
+        }
+        window.location.hash = `profile?id=${localStorage.userID || sessionStorage.userID}`;
+        window.location.reload();
+
+    }
+
 }
