941ffc99207cd523308e672993bc766f2829b6d5
Shrimp <lcamara0916@gmail.com>
2022-06-14 13:13:24 +0000

    Removed old pages directory from the project

--- WEB-PAGE/scripts/pages-old/controllers/404.js
+++
@@ -1,8 +0,0 @@
-export default class NotFound {
-    constructor() {
-        document.title = 'My Shows List | 404 - Not Found';
-        setTimeout(() => {
-            window.location.hash = '#home';
-        }, 5000);
-    }
-}
