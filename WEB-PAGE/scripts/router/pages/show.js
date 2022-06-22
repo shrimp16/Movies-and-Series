636e91810984c262ed6ec519bd0b831959bcfcb2
Shrimp <lcamara0916@gmail.com>
2022-06-22 13:26:44 +0000

    Started implementing the show page, the web page is now correctly fetching the data from the server

---
+++ WEB-PAGE/scripts/router/pages/show.js
@@ -0,0 +1,43 @@
+let showData = {};
+
+export default class Show {
+
+    constructor() {
+        this.body = document.getElementById('body');
+
+        this.url = window.location.href;
+
+        const [hash, query] = this.url.split('#')[1].split('?');
+        this.params = Object.fromEntries(new URLSearchParams(query));
+
+        this.id = this.params.id;
+        console.log(this.id);
+
+        this.getData(this.id);
+    }
+
+    async getData(id) {
+
+        await fetch(`http://192.168.1.103:50000/content/${id}`)
+            .then(async (response) => {
+                if (response.status === 404) {
+                    window.location.hash = '404';
+                    return;
+                }
+                await response.json()
+                    .then(async (response) => {
+                        showData.title = response[0].title;
+                        showData.description = response[0].text;
+                        showData.rate = response[0].rate;
+
+                        await fetch(`http://192.168.1.103:50000/image/${response[0].image}`)
+                            .then(image => image.blob()
+                                .then((image) => {
+                                    showData.image = URL.createObjectURL(image);
+                                }))
+                    })
+            })
+        console.log(showData);
+    }
+
+}
