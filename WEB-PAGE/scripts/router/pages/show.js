42634640469ec4e559d4d4d8035690928c26eace
Shrimp <lcamara0916@gmail.com>
2022-06-25 16:58:29 +0000

    Comments now have some little differences based on if they are liked or not, still needs some fixes

--- WEB-PAGE/scripts/router/pages/show.js
+++ WEB-PAGE/scripts/router/pages/show.js
@@ -74,59 +74,21 @@ export default class Show {
                         </div>
                         <div class="comment">
                             <img src="../../images/default-user-image.png">
-                            <p>This is a very nice very good comment omg this nees to be a lot bigger now omg wow wtf lol lmao xd xd xd </p>
+                            <p>This is how a liked comment is supposed to look like </p>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 16 16" fill="red">
+                                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                            </svg>
                         </div>
                         <div class="comment">
                             <img src="../../images/default-user-image.png">
-                            <p>This is a very nice very good comment omg this nees to be a lot bigger now omg wow wtf lol lmao xd xd xd </p>
+                            <p>This is how a not liked comment is supposed to look like</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 16 16">
+                                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                            </svg>
                         </div>
                         <div class="comment">
                             <img src="../../images/default-user-image.png">
-                            <p>This is a very nice very good comment omg this nees to be a lot bigger now omg wow wtf lol lmao xd xd xd </p>
-                        </div>
-                        <div class="comment">
-                            <img src="../../images/default-user-image.png">
-                            <p>This is a very nice very good comment omg this nees to be a lot bigger now omg wow wtf lol lmao xd xd xd </p>
-                        </div>
-                        <div class="comment">
-                            <img src="../../images/default-user-image.png">
-                            <p>This is a very nice very good comment omg this nees to be a lot bigger now omg wow wtf lol lmao xd xd xd </p>
-                        </div>
-                        <div class="comment">
-                            <img src="../../images/default-user-image.png">
-                            <p>This is a very nice very good comment omg this nees to be a lot bigger now omg wow wtf lol lmao xd xd xd </p>
-                        </div>
-                        <div class="comment">
-                            <img src="../../images/default-user-image.png">
-                            <p>This is a very nice very good comment omg this nees to be a lot bigger now omg wow wtf lol lmao xd xd xd </p>
-                        </div>
-                        <div class="comment">
-                            <img src="../../images/default-user-image.png">
-                            <p>This is a very nice very good comment omg this nees to be a lot bigger now omg wow wtf lol lmao xd xd xd </p>
-                        </div>
-                        <div class="comment">
-                            <img src="../../images/default-user-image.png">
-                            <p>This is a very nice very good comment omg this nees to be a lot bigger now omg wow wtf lol lmao xd xd xd </p>
-                        </div>
-                        <div class="comment">
-                            <img src="../../images/default-user-image.png">
-                            <p>This is a very nice very good comment omg this nees to be a lot bigger now omg wow wtf lol lmao xd xd xd </p>
-                        </div>
-                        <div class="comment">
-                            <img src="../../images/default-user-image.png">
-                            <p>This is a very nice very good comment omg this nees to be a lot bigger now omg wow wtf lol lmao xd xd xd </p>
-                        </div>
-                        <div class="comment">
-                            <img src="../../images/default-user-image.png">
-                            <p>This is a very nice very good comment omg this nees to be a lot bigger now omg wow wtf lol lmao xd xd xd </p>
-                        </div>
-                        <div class="comment">
-                            <img src="../../images/default-user-image.png">
-                            <p>This is a very nice very good comment omg this nees to be a lot bigger now omg wow wtf lol lmao xd xd xd </p>
-                        </div>
-                        <div class="comment">
-                            <img src="../../images/default-user-image.png">
-                            <p>This is a very nice very good comment omg this nees to be a lot bigger now omg wow wtf lol lmao xd xd xd </p>
+                            <p>This is how a not liked comment is supposed to look like to the outside</p>
                         </div>
                         <div class="white-space"></div>
                         <div class="create-comment">
