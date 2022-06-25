ebb7e0090896f67af14c2f66dbbb9b9935859e47
Shrimp <lcamara0916@gmail.com>
2022-06-25 16:41:06 +0000

    Created a view for when there's no comments yet

--- WEB-PAGE/scripts/router/pages/show.js
+++ WEB-PAGE/scripts/router/pages/show.js
@@ -65,6 +65,13 @@ export default class Show {
                     <div class="bar"></div>
 
                     <div class="show-comments" id="comments-view">
+                        <div class="no-comment">
+                            <p>No comments yet!</p>
+                            <svg width="32" height="32" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                <path d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.498 3.498 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.498 4.498 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
+                            </svg>
+                        </div>
                         <div class="comment">
                             <img src="../../images/default-user-image.png">
                             <p>This is a very nice very good comment omg this nees to be a lot bigger now omg wow wtf lol lmao xd xd xd </p>
