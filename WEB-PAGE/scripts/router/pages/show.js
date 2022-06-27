2a685c8e1cead483c51cf7c31bcef10161e2774c
Shrimp <lcamara0916@gmail.com>
2022-06-27 12:45:11 +0000

    Users can now write comments

--- WEB-PAGE/scripts/router/pages/show.js
+++ WEB-PAGE/scripts/router/pages/show.js
@@ -129,8 +129,8 @@ export default class Show {
         <div class="white-space"></div>
         <div class="create-comment">
             <!---<img src="../../images/default-user-image.png">-->
-            <textarea class="write-comment" rows="3" cols="50"></textarea>
-            <button>
+            <textarea class="write-comment" rows="3" cols="50" id="comment-input">Write a comment!</textarea>
+            <button id="send-comment">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
                     <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
                 </svg>
@@ -139,6 +139,29 @@ export default class Show {
         `
 
         commentsView.innerHTML = HTML;
+
+        document.getElementById('comment-input').addEventListener('click', () => {
+            if(document.getElementById('comment-input').value === 'Write a comment!'){
+                document.getElementById('comment-input').value = '';
+            }
+        })
+
+        document.getElementById('send-comment').addEventListener('click', () => {
+            document.getElementById('comment-input').value
+            fetch('http://192.168.1.103:50000/add-comment', {
+                method: 'POST',
+                body: JSON.stringify({
+                    'ownerID': localStorage.userID || sessionStorage.userID,
+                    'contentID': this.id,
+                    'text': document.getElementById('comment-input').value
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8'
+                }
+            }).then(() => {
+                window.location.reload();
+            })
+        })
 
     }
 
